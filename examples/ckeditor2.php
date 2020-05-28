282ee67221d2b7d0d678de36562f11d697f735f4
kukrik <tiit.papp@gmail.com>
2020-05-29 01:10:39 +0300

    Completed and tested

---
+++ examples/ckeditor2.php
@@ -0,0 +1,43 @@
+<?php
+require('qcubed.inc.php');
+
+use QCubed as Q;
+use QCubed\Project\Control\ControlBase;
+use QCubed\Project\Control\FormBase as Form;
+use QCubed\Project\Control\Button;
+use QCubed\Control\Panel;
+use QCubed\Event\Click;
+use QCubed\Action\Ajax;
+
+/**
+ * Class SampleForm2
+ *
+ * This example demonstrates how to call an initialization function to customize the ck editor
+ */
+
+class SampleForm2 extends Form
+{
+	protected $txtEditor;
+	protected $btnSubmit;
+	protected $pnlResult;
+
+	protected function Form_Create()
+	{
+		$this->txtEditor = new Q\Plugin\CKEditor($this);
+		$this->txtEditor->Text = '<b>Something</b> to start with.';
+		$this->txtEditor->Configuration = 'ckConfig';
+
+		$this->btnSubmit = new Button($this);
+		$this->btnSubmit->Text = "Submit";
+		$this->btnSubmit->AddAction(new Click(), new Ajax('submit_click'));
+
+		$this->pnlResult = new Panel($this);
+		$this->pnlResult->HtmlEntities = true;
+	}
+
+	protected function submit_click($strFormId, $strControlId, $param) {
+		$this->pnlResult->Text = $this->txtEditor->Text;
+	}
+}
+
+SampleForm2::Run('SampleForm2');
---
+++ src/CKEditorBase.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * This file contains the QCKEditor Class.
+ */
+
+namespace QCubed\Plugin;
+
+use QCubed as Q;
+use QCubed\Exception\Caller;
+use QCubed\Exception\InvalidCast;
+use QCubed\Project\Control\ControlBase;
+use QCubed\Project\Control\FormBase;
+use QCubed\Project\Control\TextBox;
+use QCubed\Control\TextBoxBase;
+use QCubed\Type;
+
+
+/**
+ * Class CKEditorBase: For creating a Rich text editor with CKEditor
+ *
+ * @package QCubed\Plugin
+ *
+ * @property-write string $ReadyFunction JS function to pass to the ckeditor creation instance
+ * @property-write string $Configuration Configuration options to pass to the ckeditor instance
+ */
+class CKEditorBase extends TextBoxBase {
+
+	protected $strJsReadyFunc = 'function(){}';
+	protected $strConfiguration = '{}';
+
+	public function __construct($objParentObject, $strControlId = null) {
+		parent::__construct($objParentObject, $strControlId);
+		$this->registerFiles();
+
+		$this->strCrossScripting = TextBox::XSS_ALLOW;
+		$this->strTextMode = TextBoxBase::MULTI_LINE;
+	}
+
+	protected function registerFiles() {
+
+		$this->AddJavascriptFile(QCUBED_CKEDITOR_ASSETS_URL . "/php/QCKSetup.js.php");
+		$this->AddJavascriptFile(QCUBED_VENDOR_URL . "/ckeditor/ckeditor/ckeditor.js");
+		$this->AddJavascriptFile(QCUBED_VENDOR_URL . "/ckeditor/ckeditor/adapters/jquery.js");
+	}
+
+	public function getJqSetupFunction() {
+		return 'ckeditor';
+	}
+
+	// currently cannot use ajax
+	public function GetControlJavaScript() {
+		$strFormId = $this->Form->FormId;
+		$strControlId = $this->strControlId;
+		$strReadyFunc = 'null';
+		if ($this->strJsReadyFunc) {
+			$strReadyFunc = $this->strJsReadyFunc;
+		}
+		$strJs = "function() {qcubed.qckeditor(this, '{$strFormId}', '{$strControlId}', {$strReadyFunc});}";
+		return sprintf('jQuery("#%s").%s(%s, %s)', $this->getJqControlId(), $this->getJqSetupFunction(), $strJs, $this->strConfiguration);
+	}
+
+	public function GetEndScript() {
+		return  $this->GetControlJavaScript() . '; ' . parent::GetEndScript();
+	}
+
+	public function __set($strName, $mixValue) {
+
+		//$this->blnModified = true;
+		switch ($strName) {
+			case "ReadyFunction":
+				// The name of a javascript function to call after the CKEditor instance is ready, so that you can do further initialization
+				// This function will receive the formId and controlId as parameters, and "this" will be the ckeditor instance.
+				try {
+					$this->strJsReadyFunc = Type::Cast($mixValue, Type::STRING);
+					break;
+				} catch (InvalidCast $objExc) {
+					$objExc->incrementOffset();
+					throw $objExc;
+				}
+				break;
+
+			case "Configuration":
+				// The configuration string. Could be a name of an object, or a javascript object (sourrounded by braces {})
+				try {
+					$this->strConfiguration = Type::Cast($mixValue, Type::STRING);
+					break;
+				} catch (InvalidCast $objExc) {
+					$objExc->incrementOffset();
+					throw $objExc;
+				}
+				break;
+
+
+			default:
+				try {
+					parent::__set($strName, $mixValue);
+				} catch (Caller $objExc) {
+					$objExc->incrementOffset();
+					throw $objExc;
+				}
+				break;
+
+		}
+	}
+
+
+}
