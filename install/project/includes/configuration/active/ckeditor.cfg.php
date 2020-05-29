1dc671e099ba3401a2c2f70c02ce173d5841ed6c
kukrik <tiit.papp@gmail.com>
2020-05-29 21:45:26 +0300

    Init

---
+++ install/project/includes/configuration/active/ckeditor.cfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * ckeditor defines
+ */
+
+define('QCUBED_CKEDITOR_URL', dirname(QCUBED_PROJECT_DIR) . '/vendor/kukrik/ckeditor');
+define('QCUBED_CKEDITOR_ASSETS_URL', dirname(QCUBED_BASE_URL) . '/kukrik/ckeditor/assets');
+
+
+
+
